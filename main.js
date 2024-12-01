7fdbed24b14a8ea8080a20aa7dbe1d0b0b6424f9
Atilon.dev40 <129638566+pelapato@users.noreply.github.com>
2024-12-01 18:32:58 +0000

    atualizacao

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const canvas = document.querySelector("canvas")
 const ctx=canvas.getContext("2d")
 
-
 //Feramentas
 const button = [...document.querySelectorAll("button")]
 const lapis = document.querySelector("button#lapis")
@@ -12,17 +11,22 @@ const feramentas=()=>{
   button.map((e)=>{
     e.addEventListener("click",()=>{
       e.classList.toggle("btn_ativo")
-      console.log(e.classList)
-    })
+    })    
   })
 }
+
 feramentas()
+
 
 const sistema=(px,py)=>{
   ctx.fillStyle="red"
   ctx.arc(px - canvas.offsetLeft,py - canvas.offsetTop,1,1,360,false)
   ctx.fill()
 }
+
+canvas.addEventListener("mousemove",(evt)=>{
+  sistema(evt.clientX,evt.clientY)
+})
 
 canvas.addEventListener('touchmove',(evt)=>{
   let x = evt.touches[0].clientX
